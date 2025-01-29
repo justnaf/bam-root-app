9b833cea0f674d7e51e65e6a1bb2aea9d265d1e2
justnaf <naufalaf86@gmail.com>
2025-01-29 19:19:24 +0700

    Modify User Count

--- resources/views/accounts/index.blade.php
+++ resources/views/accounts/index.blade.php
@@ -7,7 +7,7 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="grid grid-cols-2">
+            <div class="grid grid-cols-2 gap-3 mb-3">
                 @foreach ($roleCounts as $role => $count)
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-2">
